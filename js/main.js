88dfbc3abdb5f7c9ef2cade4f31bd6f4df2d11ed
Yann Gouffon <yago@yago-art.ch>
2015-03-21 16:47:08 +0100

    Update 2015-03-21T15:47:05.524Z

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+'use strict';
+
+(function($, Reveal){
+  function focusCode (event) {
+    var $this = $(event.fragment);
+    if (typeof $this.attr("data-focus") !== typeof undefined) {
+      var lineData = $this.data('focus');
+      if (String(lineData).match(/\,/g)) {
+        var lines = lineData.split(',');
+        $this.parents('section').find('pre .rainbow-line').css('opacity', '0.3');
+        lines.forEach(function(line) {
+          $this.parents('section').find('pre .rainbow-line-'+line).css('opacity', '1');
+        });
+      } else {
+        $this.parents('section').find('pre .rainbow-line').css('opacity', '0.3');
+        $this.parents('section').find('pre .rainbow-line-'+lineData).css('opacity', '1');
+      }
+    }
+  }
+  Reveal.addEventListener( 'slidechanged', function( event ) {
+    $(event.currentSlide).find('pre .rainbow-line').css('opacity', '1');
+  });
+  Reveal.addEventListener( 'fragmentshown', function( event ) {
+    focusCode(event);
+  });
+  Reveal.addEventListener( 'fragmenthidden', function( event ) {
+    focusCode(event);
+  });
+}(jQuery, Reveal));
 emojify.setConfig({
     img_dir          : 'img/emoji',  // Directory for emoji images
     ignored_tags     : {                // Ignore the following tags
