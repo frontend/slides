2b46fdfc1cbc55f8b5ea3760d19d5a5383f9a624
Yann Gouffon <yago@yago-art.ch>
2015-03-23 07:59:24 +0100

    Update 2015-03-23T06:59:21.011Z

--- js/main.js
+++ js/main.js
@@ -163,3 +163,67 @@ if (window.Rainbow) window.Rainbow.linecount = (function(Rainbow) {
         $block.replaceWith($lines);
     });
 })(window.Rainbow);
+'use strict';
+
+(function($, Reveal){
+  function launchTerminal (event) {
+    var $this = $(event.fragment);
+    if (typeof $this.attr("data-terminal") !== typeof undefined) {
+      var $terminal = $($this.data("terminal")),
+          command = $terminal.data('command'),
+          result = $terminal.data('result');
+
+      $('<span class="terminal-render"></span>').insertAfter($terminal);
+      var $render = $terminal.next('.terminal-render');
+
+      $render.typed({
+          strings: [''+command+''],
+          typeSpeed: 100,
+          backDelay: 500,
+          loop: false,
+          contentType: 'html', // or text
+          // defaults to false for infinite loop
+          loopCount: true,
+          callback: function(){
+            if (String(result).match(/\,/g)) {
+              var results = result.split(',');
+              for (var i = 0; i < results.length; i++) {
+                setTimeout(function(x) { return function() {
+                  $render.append('<br>'+results[x]);
+                }; }(i), 300*i);
+              }
+            } else {
+              setTimeout(function(){
+                $render.append('<br>'+result);
+              }, 500);
+            }
+            $render.next('.typed-cursor').remove();
+          },
+          resetCallback: function() {console.log(this);}
+      });
+    }
+  }
+
+  function resetTerminal (event) {
+    var $this = $(event.fragment);
+    if (typeof $this.attr("data-terminal") !== typeof undefined) {
+      var $terminal = $($this.data("terminal"));
+      $terminal.next('.terminal-render').remove();
+      $terminal.next('.typed-cursor').remove();
+    }
+  }
+
+  Reveal.addEventListener( 'fragmentshown', function(event) {
+    launchTerminal(event);
+  });
+  Reveal.addEventListener( 'fragmenthidden', function( event ) {
+    resetTerminal(event);
+  });
+}(jQuery, Reveal));
+'use strict';
+
+(function($){
+  $('.reveal section:first-child').find('h1, h2, h3').slabText({
+      'viewportBreakpoint':380
+  });
+}(jQuery));
